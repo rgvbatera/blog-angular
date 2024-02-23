b97e08a3aaeeb3fbb6a8cb34d04d4c11acf1e3a6
rgvbatera <rgbatera@gmail.com>
2024-02-22 21:19:02 -0300

    blog criado

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,14 @@
+export const dataFake = [
+  {
+    "id":"1",
+    "title": "TECNOLOGIA DA INFORMAÇÃO",
+    "description": "O que esperar da tecnologia da informação",
+    "photoCover":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQvdnQhcQ2vJa5kmd-ZOdFukKI2cSTjFZwqyA&usqp=CAU"
+  },
+  {
+    "id":"2",
+    "title": "Ministério da Ciência, Tecnologia e Inovação",
+    "description": "",
+    "photoCover":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQk6BrtL1WjEBiWuCLip_Ck3e8nhII8hlOILQ&usqp=CAU"
+  }
+]
